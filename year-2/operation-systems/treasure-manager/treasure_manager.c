0db72e8c01e954eeced370f35178ed33d76a9636
Diana Plesa <diana.plesa04@gmail.com>
2025-04-12 16:16:38 +0300

    added remove treasure ,remove hunt and some data validation

--- year-2/operation-systems/treasure-manager/treasure_manager.c
+++ year-2/operation-systems/treasure-manager/treasure_manager.c
@@ -20,7 +20,7 @@ typedef struct
 	char treasure_id[11];
 	char username[35];
 	GPS coord;
-	char clue[50];
+	char clue[51];
 	int value;
 }TREASURE;
 
@@ -30,7 +30,6 @@ void create_directory(char pathname[])
 	
 	if (status != 0 && errno != EEXIST)
 	{
-		//printf("Error creating directory\n");
 		perror("Error creating directory");
 		exit(-1);
 	}
@@ -41,7 +40,6 @@ int open_file_write(char filename[])
 	int f_out = 0;
 	if ((f_out = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1)
 	{
-		//printf("Error opening file\n");
 		perror("Error opening file");
 		exit(-1);
 	}
@@ -54,7 +52,6 @@ int open_file_read(char filename[])
 	int f_out = 0;
 	if ((f_out = open(filename, O_RDONLY)) == -1)
 	{
-		//printf("Error opening file\n");
 		perror("Error opening file");
 		exit(-1);
 	}
@@ -81,41 +78,52 @@ void file_exists(char* filepath)
 	}
 }
 
-// void dir_exists(char *dirpath)
-// {
-// 	DIR* dir = opendir(dirpath);
-// 	if (dir == NULL)
-// 	{
-// 		// if (errno == ENOENT)
-// 		// {
-// 		// 	printf("Directory does not exist\n");
-// 		// 	exit(-1);
-// 		// }
-
-// 		perror("Error opening directory\n");
-// 		exit(-1);
-// 	}
-
-// }
-
 TREASURE read_treasure()
 {
 	TREASURE tr;
 
-	printf("Username: ");
-	scanf("%s", tr.username);
-	printf("Treasure id: ");
-	scanf("%s", tr.treasure_id);
-	printf("X: ");
+	printf("Username (cannot contain whitespaces) (34 characters max): ");
+	scanf("%34s", tr.username);
+	printf("Treasure id (must contain at least one number) (10 characters max): ");
+	scanf("%10s", tr.treasure_id);
+	int id_format = 1;
+	for (int i = 0; i < strlen(tr.treasure_id); i++)
+	{
+		if (strchr("0123456789", tr.treasure_id[i]))
+			id_format = 0;
+	}
+	if (id_format)
+	{
+		printf("Invalid treasure_id format\n");
+		exit(-1);
+	}
+	printf("X (must be between -500 and 500): ");
+	getchar();
 	scanf("%f", &tr.coord.x);
-	printf("Y: ");
+	if (tr.coord.x < -500 || tr.coord.x > 500)
+	{
+		printf("Invalid X coordinate\n");
+		exit(-1);
+	}
+	printf("Y (must be between -500 and 500): ");
+	getchar();
 	scanf("%f", &tr.coord.y);
-	printf("Clue: ");
+	if (tr.coord.y < -500 || tr.coord.y > 500)
+	{
+		printf("Invalid Y coordinate\n");
+		exit(-1);
+	}
+	printf("Clue (50 characters max): ");
 	getchar();
 	fgets(tr.clue, sizeof(tr.clue), stdin);
 	tr.clue[strcspn(tr.clue, "\n")] = '\0';
-	printf("Value: ");
+	printf("Value (must be between 5 and 12000): ");
 	scanf("%d", &tr.value);
+	if (tr.value < 5 || tr.value > 12000)
+	{
+		printf("Invalid treasure value\n");
+		exit(-1);
+	}
 
 	return tr;
 }
@@ -135,8 +143,16 @@ void get_time(struct tm *local_time, char* final_time)
 	strftime(final_time, 26, "%Y-%m-%d %H:%M:%S", local_time);
 }
 
-void write_log(int f_out, TREASURE tr, char action[])
+void write_log(char* pathname, char* username, char* id, char action[])
 {
+	char logpath[100];
+	strcpy(logpath, pathname);
+	strcat(logpath, "/logged_hunt.txt");
+
+	int f_log = 0;
+
+	f_log = open_file_write(logpath);
+
 	time_t now = time(NULL);
 	struct tm *local_time = localtime(&now);
 	char final_time[50];
@@ -144,13 +160,15 @@ void write_log(int f_out, TREASURE tr, char action[])
 	get_time(local_time, final_time);
 	char log_entry[200];
 
-	snprintf(log_entry, sizeof(log_entry), "%s %s: %s %s\n", final_time, tr.username, action, tr.treasure_id);
+	snprintf(log_entry, sizeof(log_entry), "%s %s: %s %s\n", final_time, username, action, id);
 
-	if (write(f_out, log_entry, strlen(log_entry)) == -1)
+	if (write(f_log, log_entry, strlen(log_entry)) == -1)
 	{
 		perror("Error writing in log file\n");
 		exit(-1);
 	}
+	close_file(f_log);
+
 }
 
 void create_symlink(char logpath[], char hunt_id[])
@@ -174,7 +192,6 @@ void concat_path(char* path1, char *path2)
 
 void add_hunt(char hunt_id[], char pathname[])
 {
-	//strcat(pathname, hunt_id);
 	concat_path(pathname, hunt_id);
 	create_directory(pathname);
 
@@ -186,21 +203,18 @@ void add_hunt(char hunt_id[], char pathname[])
 	strcpy(logpath, pathname);
 	strcat(logpath, "/logged_hunt.txt");
 
-	int f_out = 0, f_log = 0;
+	int f_out = 0;
 
 	f_out = open_file_write(filepath);
-	f_log = open_file_write(logpath);
 
 	TREASURE tr;
 	tr = read_treasure();
 
 	write_treasure(f_out, tr);
-	write_log(f_log, tr, "add");
+	write_log(pathname, tr.username, tr.treasure_id, "add");
 
 	create_symlink(logpath, hunt_id);
 	close_file(f_out);
-	close_file(f_log);
-
 }
 
 void print_treasure(TREASURE tr)
@@ -281,11 +295,151 @@ void view_tr(char* hunt_id, char* tr_id, char* pathname)
 	if (strcmp(tr.treasure_id, tr_id) != 0)
 	{
 		printf("Treasure id could not be found\n");
+		close_file(f_out);
 		return;
 	}
 	print_treasure(tr);
-	
+	close_file(f_out);
+
 }
+
+void remove_tr(char* hunt_id, char* tr_id, char* pathname)
+{
+	char username[35];
+	printf("Username: ");
+	scanf("%s", username);
+	int f_out = 0, f_temp = 0;
+	concat_path(pathname, hunt_id);
+	char filepath[100], temppath[100];
+	strcpy(filepath, pathname);
+	strcat(filepath, "/treasures.bin");
+	strcpy(temppath, filepath);
+	strcat(temppath, ".bak");
+	f_out = open_file_read(filepath);
+	f_temp = open_file_write(temppath);
+	TREASURE tr;
+	int found = 0;
+
+	while (read(f_out, &tr, sizeof(TREASURE)) == sizeof(TREASURE))
+	{
+		if (strcmp(tr_id, tr.treasure_id) != 0)
+		{
+			write_treasure(f_temp, tr);
+		}
+		else found = 1;
+	}
+
+	close_file(f_temp);
+	close_file(f_out);
+
+
+	if (found == 0)
+	{
+		printf("Treasure id could not be found\n");
+
+		if (remove(temppath) != 0)
+		{
+			perror("Could not remove file\n");
+			exit(-1);
+		}
+		return;
+	}
+	f_temp = open_file_read(temppath);
+
+	if ((f_out = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1)
+	{
+		perror("Error opening file");
+		exit(-1);
+	}
+
+	while (read(f_temp, &tr, sizeof(TREASURE)) == sizeof(TREASURE))
+	{
+		write_treasure(f_out, tr);
+	}
+	close_file(f_out);
+	close_file(f_temp);
+
+	if (remove(temppath) != 0)
+	{
+		perror("Could not remove file\n");
+		exit(-1);
+	}
+	char action[35];
+	strcpy(action, "remove from ");
+	strcat(action, hunt_id);
+
+	write_log(pathname, username, tr_id, action);
+}
+
+void remove_in_dir(char* dirpath, char* pathname)
+{
+	DIR *dir = opendir(dirpath);
+	if (dir == NULL)
+	{
+		perror("Could not open directory");
+		exit(-1);
+	}
+
+	char filepath[100];
+	
+	struct dirent* entry;
+	errno = 0;
+
+	while ((entry = readdir(dir)) != NULL)
+	{
+		//printf("%s\n", entry->d_name);
+		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+		continue;
+		
+		strcpy(filepath, dirpath);
+		concat_path(filepath, entry->d_name);
+		//printf("%s\n", filepath);
+		if (remove(filepath) != 0)
+		{
+			perror("Could not remove file inside hunt directory");
+			if (closedir(dir) != 0)
+			{
+				perror("Error closing directory\n");
+				exit(-1);
+			}
+			exit(-1);
+		}
+	}
+
+		if (closedir(dir) != 0)
+	{
+		perror("Error closing directory\n");
+		exit(-1);
+	}
+
+}
+
+void remove_hunt(char* hunt_id, char* pathname)
+{
+	char dirpath[100], sympath[100], symname[50];
+	strcpy(dirpath, pathname);
+	strcpy(sympath, pathname);
+	strcpy(symname, "logged_hunt-");
+	strcat(symname, hunt_id);
+	concat_path(dirpath, hunt_id);
+
+	remove_in_dir(dirpath, pathname);
+
+	concat_path(sympath, symname);
+	if (remove(dirpath) != 0)
+	{
+		perror("Could not remove directory");
+		exit(-1);
+	}
+
+	if (unlink(sympath) != 0)
+	{
+		perror("Error remove symlink");
+		exit(-1);
+	}
+
+}
+
 
 int main(int argc, char** argv)
 {
@@ -306,7 +460,7 @@ int main(int argc, char** argv)
 	}
 	if (strcmp(argv[1], "--add") == 0)
 	{
-		if (argc == 2)
+		if (argc != 3)
 		{
 			printf("Format should be ./exec --add <hunt_id>\n");
 			return 0;
@@ -318,7 +472,7 @@ int main(int argc, char** argv)
 	}
 	else if (strcmp(argv[1], "--list") == 0)
 	{
-		if (argc == 2)
+		if (argc != 3)
 		{
 			printf("Format should be ./exec --list <hunt_id>\n");
 			return 0;
@@ -331,7 +485,7 @@ int main(int argc, char** argv)
 
 	else if (strcmp(argv[1], "--view") == 0)
 	{
-		if (argc == 2 || argc == 3)
+		if (argc != 4)
 		{
 			printf("Format should be ./exec --view <hunt_id> <id>\n");
 			return 0;
@@ -343,6 +497,36 @@ int main(int argc, char** argv)
 		strcpy(tr_id, argv[3]);
 
 		view_tr(hunt_id, tr_id, pathname);
+	}
+
+	else if (strcmp(argv[1], "--remove_treasure") == 0)
+	{
+		if (argc != 4)
+		{
+			printf("Format should be ./exec --remove_treasure <hunt_id> <id>\n");
+			return 0;
+		}
+
+		char hunt_id[35];
+		char tr_id[11];
+		strcpy(hunt_id, argv[2]);
+		strcpy(tr_id, argv[3]);
+
+		remove_tr(hunt_id, tr_id, pathname);
+	}
+
+		else if (strcmp(argv[1], "--remove_hunt") == 0)
+	{
+		if (argc != 3)
+		{
+			printf("Format should be ./exec --remove_hunt <hunt_id>\n");
+			return 0;
+		}
+
+		char hunt_id[35];
+		strcpy(hunt_id, argv[2]);
+
+		remove_hunt(hunt_id, pathname);
 	}
 
 	//!!!! basic data validation
